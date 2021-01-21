86b232d77cff213cb8e20126aa3fc7eac2585b98
Lucas Felix <lucasfelixdev@gmail.com>
2021-01-21 17:39:20 -0300

    Finalizing initial settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
@@ -13,3 +13,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
